bd599d8fd2e1f6df55a3ea4c732c0606ae226b84
ES00500546 - ANDREU MARINER PIQUERES <es00500546@aieamariner.local>
2025-11-24 16:56:41 +0100

    Initial commit: Migrate Webflow to Astro/Svelte

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,16 @@
+<script>
+  // Script logic can be added here
+</script>
+
+<div class="fixed-top">
+  <div role="banner" class="navbar w-nav">
+    <div class="container w-container">
+      <div class="w-layout-grid navbar-grid">
+        <a href="/" aria-current="page" class="brand w-nav-brand w--current">
+          <img loading="lazy" src="/images/logo-maestrat-web.png" alt="" class="navbar-logo">
+          <img loading="lazy" src="/images/Sunny-Lake-Black.svg" alt="Logo" class="navbar-logo-hide">
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
