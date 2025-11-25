d65fcb057dfc312e3ae37ee0459dd02ab3f2a7db
ES00500546 - ANDREU MARINER PIQUERES <es00500546@aieamariner.local>
2025-11-25 16:49:25 +0100

    chore: switch to static output and remove netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
-import netlify from '@astrojs/netlify';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
-  adapter: netlify()
+  output: 'static',
+  integrations: [svelte()]
 });
